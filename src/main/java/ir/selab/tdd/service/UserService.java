d8d2c07fce9bbf4c75dd2405fa6ebcc873904509
Amin Ahmadzadeh <s.ahmadzadeh@emofid.com>
2024-11-08 22:40:23 +0330

    feat: resolves #5

--- src/main/java/ir/selab/tdd/service/UserService.java
+++ src/main/java/ir/selab/tdd/service/UserService.java
@@ -36,12 +36,12 @@ public class UserService {
 
     public boolean removeUser(String username) {
         // TODO: implement
-        return false;
+        return repository.removeUser(username);
     }
 
     public List<User> getAllUsers() {
         // TODO: implement
-        return null;
+        return repository.getAllUsers();
     }
 
     public boolean changeUserEmail(String username, String newEmail) {
